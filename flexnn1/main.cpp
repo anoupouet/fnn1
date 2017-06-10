83c0d1ef5c72648ee03b6eb77deedc053139c29c
Alexander Lyashevsky <anopouet@gmail.com>
2017-06-09 21:13:32 -0700

    Initial Commit

---
+++ flexnn1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  flexnn1
+//
+//  Created by Alexander Lyashevsky on 6/9/17.
+//  Copyright © 2017 Alexander Lyashevsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
