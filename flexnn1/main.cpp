ab079b0f04746df94a4da4bbee2cceb41183bc1e
Alexander Lyashevsky <anopouet@gmail.com>
2017-06-09 22:21:47 -0700

    arg parser

--- flexnn1/main.cpp
+++ flexnn1/main.cpp
@@ -6,15 +6,80 @@
 //  Copyright © 2017 Alexander Lyashevsky. All rights reserved.
 //
 
+/*In the C++ header file "unistd.h", function getopt can get the arguments with different options.*/
 #include <iostream>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define NUM_TYPES   5
 enum {INT8, INT16, INT32, FP16, FP32};
 extern "C" const char *precisionName[NUM_TYPES] = {"INT8", "INT16", "INT32", "FP16", "FP32"};
 
-
 extern "C" int mainLoop(int unitPrecision,
              int targetNumClocks);
+
+static
+void showhelpinfo(char *s);
+using namespace std;
+int myParser (int argc, char *argv[])
+{
+    char tmp;
+    /*if the program is ran witout options ,it will show the usgage and exit*/
+    if(argc == 1)
+    {
+        showhelpinfo(argv[0]);
+        exit(1);
+    }
+    /*use function getopt to get the arguments with option."hp:c:m:v" indicate
+     that option h,v are the options without arguments while u,p,s are the
+     options with arguments*/
+    while((tmp=getopt(argc,argv,"hp:c:m:v"))!=-1)
+    {
+        switch(tmp)
+        {
+                /*option h show the help infomation*/
+            case 'h':
+                showhelpinfo(argv[0]);
+                break;
+                /*option c */
+            case 'c':
+                cout<<"Target clock is "<<optarg<<endl;
+                break;
+                /*option p */
+            case 'p':
+                cout<<"Target precision is "<<optarg<<endl;
+                break;
+                /*option m */
+            case 'm':
+                cout<<"Model file is "<<optarg<<endl;
+                break;
+                /*option v show the version infomation*/
+            case 'v':
+                cout<<"The current version is 0.1"<<endl;
+                break;
+                /*invail input will get the heil infomation*/
+            default:
+                showhelpinfo(argv[0]);
+                break;
+        }
+    }
+    
+    return (0);
+}
+
+/*funcion that show the help information*/
+void showhelpinfo(char *s)
+{
+    cout<<"Usage:   "<<s<<" [-option] [argument]"<<endl;
+    cout<<"option:  "<<"-h  show help information"<<endl;
+    cout<<"         "<<"-p  presision:  int8, int16, int32, fp16, fp32; default: fp32"<<endl;
+    cout<<"         "<<"-c  targret clock; default: 1000"<<endl;
+    cout<<"         "<<"-m  model definision file; default: flexnn.ad"<<endl;
+    cout<<"         "<<"-v  show version infomation"<<endl;
+    cout<<"example: "<<s<<" -pfp16 -c10000 -mmymodel.ad"<<endl;
+}
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -22,7 +87,7 @@ int main(int argc, const char * argv[]) {
     
     int unitPrecision = FP32;
     int targetNumClocks = 1000;
-   
+    myParser (argc,(char**)argv);
     mainLoop(unitPrecision,targetNumClocks);
     
     return 0;
